cc0993bcadd5416188a3293ee6673b10cc407b13
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2022-02-04 23:33:04 +0530

    Added:using get request method along with params

--- restapis/src/main/java/com/bridgelabz/restapis/controller/BridgelabzController.java
+++ restapis/src/main/java/com/bridgelabz/restapis/controller/BridgelabzController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.restapis.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class BridgelabzController {
     @GetMapping("/hello")
     public String getMessage(){
         return  "Hello From Bridgelabz";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String getMessageWithName(@PathVariable("name")String name){
+        return "Hello "+name+" From Bridgelabz";
     }
 
 }
